d3a0a24d3aff58fa56c0f2b1416f31afb5189ba6
Justin Wallen <wallenju@gmail.com>
2021-10-22 09:37:28 -0400

    butter dawg

--- Controls in Action/ViewController.swift
+++ Controls in Action/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TextField: UITextField!
+    @IBOutlet weak var Label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func stButton(_ sender: Any) {
+        Label.text = TextField.text
+    }
+    
+    @IBAction func ctButton(_ sender: Any) {
+        TextField.text = ""
+    }
+    
 }
+
+
 
